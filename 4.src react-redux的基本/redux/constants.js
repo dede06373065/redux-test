c0b9293455983385588bd9dcc63791baa09e410c
Jiazhen Guo <Jen06373065@163.com>
2021-08-16 14:05:04 +0930

    mapDispatchToProps的一个优化

---
+++ 4.src react-redux的基本/redux/constants.js
@@ -0,0 +1,3 @@
+//定义数据类型防止写错
+export const ADD='add'
+export const MINUS='minus'
--- src/containers/Count/Count.js
+++ src/containers/Count/Count.js
@@ -7,19 +7,33 @@ import {
 //引入connect连接redux和ui
 import {connect} from 'react-redux'
 
-function mapStateToProps(state){
-    return {count:state}
-}
-function mapDispatchToProps(dispatch){
-    return {
-    add:(data)=>{
-        dispatch(addActionCreater(data))
-    },
-    minus:(data)=>{
-        dispatch(minusActionCreater(data))
-    },
-    addAsync:data=>dispatch(addActionAsyncCreater(data,2000))
+// const mapStateToProps=state=>({count:state})
 
-}
-}
-export default connect(mapStateToProps,mapDispatchToProps)(CountUI)
+// const mapDispatchToProps=dispatch=>({
+//     add:(data)=>{
+//         dispatch(addActionCreater(data))
+//     },
+//     minus:(data)=>{
+//         dispatch(minusActionCreater(data))
+//     },
+//     addAsync:data=>dispatch(addActionAsyncCreater(data,2000))
+// })
+// export default connect(mapStateToProps,mapDispatchToProps)(CountUI)
+export default connect(
+    state=>({count:state}),
+    // dispatch=>({
+    //         add:(data)=>{
+    //             dispatch(addActionCreater(data))
+    //         },
+    //         minus:(data)=>{
+    //             dispatch(minusActionCreater(data))
+    //         },
+    //         addAsync:data=>dispatch(addActionAsyncCreater(data,2000))
+    //     })
+    //有mapDispatchToProps的精简写法
+    {
+        add:addActionCreater,//虽然没有体现传参数但是data是接收到了的
+        minus:minusActionCreater,
+        addAsync:addActionAsyncCreater
+    }
+)(CountUI)
