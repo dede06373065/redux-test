dd669b2aeed91f3155f26180ec8c297f807d4428
Jiazhen Guo <Jen06373065@163.com>
2021-08-16 10:52:56 +0930

    react-redux 第一节

--- src/components/Count/Count.jsx
+++ src/components/Count/Count.jsx
@@ -1,48 +1,28 @@
 import React, { Component } from 'react'
-import store from '../../redux/store'
-import { 
-    addActionCreater,
-    minusActionCreater,
-    addActionAsyncCreater} from '../../redux/count_action'
+
 
 export default class Count extends Component {
-    // state={
-    //     count:0
-    // } 不再有自己的状态
-    // componentDidMount(){
-    //     store.subscribe(()=>{
-    //         this.setState({})
-    //     })
-    // }
+
     add=()=>{
         const number=this.selectNumber.value
-        store.dispatch(addActionCreater(number*1))
     }
 
     minus=()=>{
-        const number=this.selectNumber.value
-        store.dispatch(minusActionCreater(number*1))
+        const number=this.selectNumber.value 
     }
 
     addOdd=()=>{
         const number=this.selectNumber.value
-        const preValue=store.getState()
-        if(preValue%2!==0){
-            store.dispatch(addActionCreater(number*1))
-        }
     }
 
     addAsync=()=>{
         const number=this.selectNumber.value
-        //setTimeout(()=>{
-            store.dispatch(addActionAsyncCreater(number*1,2000))
-        //},2000)
     }
 
     render() {
         return (
             <div>
-                <h1>Current value is :{store.getState()}</h1>
+                <h1>Current value is :???</h1>
                 <select ref={c=>this.selectNumber=c}>
                     <option value="1">1</option>
                     <option value="2">2</option>
